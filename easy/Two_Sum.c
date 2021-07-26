5f514429e06068a000fb1a93a824fd63c6a2ebc8
BU <46125548+thankdesert@users.noreply.github.com>
2021-07-26 15:12:57 +0900

    Create Two_Sum.c

---
+++ easy/Two_Sum.c
@@ -0,0 +1,26 @@
+/*
+---------------------------------------------
+Two_Sum
+from : https://leetcode.com/problems/two-sum/submissions/
+---------------------------------------------
+*/
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int, int>> v;
+        int i = 0, j;
+        for(auto item : nums)
+            v.push_back(make_pair(item, i++));
+        sort(v.begin(), v.end());
+        for(i = 0; i<nums.size() - 1; i++)
+            for(j = i+1; j<nums.size() && v[i].first + v[j].first <= target; j++)
+                if(v[i].first + v[j].first == target)
+                    return {v[i].second, v[j].second};
+        return {0, 0};
+    }
+};
