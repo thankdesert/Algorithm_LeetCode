a14989fa4f8add3eab8d789dffce49fe115f5370
BU <46125548+thankdesert@users.noreply.github.com>
2021-08-12 02:15:25 +0900

    Create Fibonacci_Number.cpp

---
+++ easy/Fibonacci_Number.cpp
@@ -0,0 +1,15 @@
+//from : https://leetcode.com/problems/fibonacci-number/
+
+
+class Solution {
+public:
+    int fib(int n) {
+        int* arr = new int[n + 1] {0, };
+        if(n == 0)
+            return 0;
+        arr[0] = 0, arr[1] = 1;
+        for(int i = 2; i <= n; i++)
+            arr[i] = arr[i-2] + arr[i-1];
+        return arr[n];
+    }
+};
