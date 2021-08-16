2b6f02fbdb242166fa5b4a84273f73cb129d50f1
BU <46125548+thankdesert@users.noreply.github.com>
2021-08-17 00:54:54 +0900

    Update Unique_Paths.cpp

--- medium/Unique_Paths.cpp
+++ medium/Unique_Paths.cpp
@@ -4,11 +4,7 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> v(m, vector<int>(n, 0));
-        for(int i = 0; i<n; i++)
-            v[0][i] = 1;
-        for(int i = 0; i<m; i++)
-            v[i][0] = 1;
+        vector<vector<int>> v(m, vector<int>(n, 1));
         
         for(int i = 1; i<m; i++) {
             for(int j = 1; j<n; j++)
