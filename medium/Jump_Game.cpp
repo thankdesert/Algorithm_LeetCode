1013e10ca807fa847fc85217790fe6031d9a2493
BU <46125548+thankdesert@users.noreply.github.com>
2021-08-16 16:35:51 +0900

    Update Jump_Game.cpp

--- medium/Jump_Game.cpp
+++ medium/Jump_Game.cpp
@@ -4,17 +4,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        bool* arr = new bool[nums.size()]{false, };
-        for(int i = 1; i <= nums[0] && i < nums.size(); i++)
-            arr[i] = true;
-        
-        for(int i = 1; i< nums.size(); i++) {
-            if(arr[i] == true) {
-                for(int j = i + 1; j <= i + nums[i] && j < nums.size(); j++)
-                    arr[j] = true;
+        int p = nums.size() - 1;
+        // O(N)
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            if (i + nums[i] >= p) {
+                p = i;
             }
-        }
-        
-        return arr[nums.size() - 1] == true ? true : false;
+        }        
+        return p == 0;
     }
 };
