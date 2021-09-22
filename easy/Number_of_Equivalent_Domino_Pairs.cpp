9e1ada8ad22e6545c2df01e9c1f29bcfe84717a4
BU <46125548+thankdesert@users.noreply.github.com>
2021-09-23 02:25:13 +0900

    Create Number_of_Equivalent_Domino_Pairs.cpp

---
+++ easy/Number_of_Equivalent_Domino_Pairs.cpp
@@ -0,0 +1,21 @@
+//from : https://leetcode.com/problems/number-of-equivalent-domino-pairs/
+
+
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        unordered_map<int,int> data;
+        for (const auto &n:dominoes) {
+            const auto[minv, maxv]=minmax_element(begin(n), end(n));
+            int key=*minv*10+*maxv;
+            data[key]++;
+        }
+        
+        int res=0;
+        for (auto it=data.begin();data.end()!=it;++it) {
+            auto key=it->first;
+            res+=data[key]*(data[key]-1)/2;
+        }
+        return res;
+    }
+};
