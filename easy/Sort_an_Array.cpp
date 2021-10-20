63522531dea856b616acc14e7d916291356ea695
BU <46125548+thankdesert@users.noreply.github.com>
2021-10-20 16:00:38 +0900

    Update Sort_an_Array.cpp

--- easy/Sort_an_Array.cpp
+++ easy/Sort_an_Array.cpp
@@ -1,6 +1,33 @@
 //from : https://leetcode.com/problems/sort-an-array/
 
 class Solution {
+    /*quick sort
+    //O(NlogN)
+    void quickSort(int l, int r, vector<int>& nums){
+        if(l>=r)
+            return;
+        int i=l,j=r;
+        
+        int pivot=nums[(l+r)/2];
+        do{
+            while(nums[i]<pivot)
+                ++i;
+            while(nums[j]>pivot)
+                --j;
+            if(i<=j){
+                int tmp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=tmp;
+                ++i;--j;
+            }
+        }while(i<=j);
+        
+        if(l<j)
+            quickSort(l, j, nums);
+        if(i<r)
+            quickSort(i, r, nums);
+    }
+    */
 public:
     vector<int> sortArray(vector<int>& nums) {
         /*bubble_sort
@@ -15,8 +42,9 @@ public:
             }
         }
         */
-     
+        
         sort(nums.begin(), nums.end()); //O(Nlog(N))
+        //quickSort(0, nums.size()-1, nums);
         return nums;
     }
 };
