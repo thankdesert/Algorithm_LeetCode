0f1dc9c72b9538539653b1c9f68e2e59acca350c
BU <46125548+thankdesert@users.noreply.github.com>
2022-01-04 14:04:22 +0900

    Update Partition_Equal_Subset_Sum.cpp

--- medium/Partition_Equal_Subset_Sum.cpp
+++ medium/Partition_Equal_Subset_Sum.cpp
@@ -21,4 +21,25 @@ public:
         }
         return k==dp[n][k]?true:false;
     }
+    
+    /*1d version
+    bool canPartition(vector<int>& nums) {
+        int sum=0;
+        for(int item:nums)
+            sum+=item;
+        if(1==sum%2)
+            return false;
+        sum/=2;
+        vector<int> dp(sum+1,0);
+        for(int i=0;nums.size()>i;++i){
+            for(int j=sum;0<j;--j){
+                if(j-nums[i]<0)
+                    continue;
+                if(dp[j-nums[i]]+nums[i]>dp[j])
+                    dp[j]=dp[j-nums[i]]+nums[i];
+            }
+        }
+        return sum==dp[sum]?true:false;
+    }
+    */
 };
