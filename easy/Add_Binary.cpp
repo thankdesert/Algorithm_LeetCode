ad4de6cc293cf2ecffc1d2a90f3012f14eb67091
BU <46125548+thankdesert@users.noreply.github.com>
2022-01-10 19:29:22 +0900

    Update Add_Binary.cpp

--- easy/Add_Binary.cpp
+++ easy/Add_Binary.cpp
@@ -35,3 +35,44 @@ public:
         return s[0] != '0' ? s : s.substr(1, s.size()-1);
     }
 };
+
+/*
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        reverse(a.begin(),a.end());
+        reverse(b.begin(),b.end());
+        string line="";
+        int i;
+        for(i=0;a.size()>i&&b.size()>i;++i){
+            line.push_back(a[i]+b[i]-'0');
+        }
+        int size;
+        a.size()>b.size()?size=a.size():size=b.size();
+        for(;size>i;++i){
+            a.size()>b.size()?line.push_back(a[i]):line.push_back(b[i]);
+        }
+        for(i=0;line.size()-1>i;++i){
+            if('2'==line[i]){
+                ++line[i+1];
+                line[i]='0';
+            }
+            else if('3'==line[i]){
+                ++line[i+1];
+                line[i]='1';
+            }
+        }
+        if('2'==line[line.size()-1]){
+            line[line.size()-1]='0';
+            line.push_back('1');
+        }
+        else if('3'==line[line.size()-1]){
+            line[line.size()-1]='1';
+            line.push_back('1');
+        }
+        reverse(line.begin(),line.end());
+        
+        return line;
+    }
+};
+*/
