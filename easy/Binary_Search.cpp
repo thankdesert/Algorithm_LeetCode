26abbcbbcfa3ee18f4bb07fc7a8f581e7e91c9c9
BU <46125548+thankdesert@users.noreply.github.com>
2022-03-26 14:54:47 +0900

    Update Binary_Search.cpp

--- easy/Binary_Search.cpp
+++ easy/Binary_Search.cpp
@@ -15,3 +15,21 @@ public:
         return -1;
     }
 };
+
+/*
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        for(int i=0,j=nums.size()-1;i<=j;){
+            int tmp=(i+j)>>1;
+            if(target==nums[tmp])
+                return tmp;
+            else if(target>nums[tmp])
+                i=tmp+1;
+            else
+                j=tmp-1;
+        }
+        return -1;
+    }
+};
+*/
