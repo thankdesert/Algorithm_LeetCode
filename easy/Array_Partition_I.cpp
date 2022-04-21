012d96b90b6e1195a3e70bf0c344a5f8985a84da
静夫 <46125548+thankdesert@users.noreply.github.com>
2022-04-21 14:38:36 +0900

    Create Array_Partition_I.cpp

---
+++ easy/Array_Partition_I.cpp
@@ -0,0 +1,16 @@
+//from : https://leetcode.com/problems/array-partition-i/
+
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        //1,2,2,5,6,6
+        if(0!=nums.size()%2)
+            return -1;
+        sort(nums.begin(),nums.end());
+        int sum=0;
+        for(int i=0;nums.size()>i;i+=2)
+            sum+=nums[i];
+        return sum;
+    }
+};
