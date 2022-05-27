f226f93e9b63cc44364523a6b76a57d06d236418
夫 <46125548+thankdesert@users.noreply.github.com>
2022-05-27 21:44:43 +0900

    Update Array_Partition_I.cpp

--- easy/Array_Partition_I.cpp
+++ easy/Array_Partition_I.cpp
@@ -5,12 +5,13 @@ class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
         //1,2,2,5,6,6
-        if(0!=nums.size()%2)
+        if (0 != nums.size() % 2) {
             return -1;
-        sort(nums.begin(),nums.end());
-        int sum=0;
-        for(int i=0;nums.size()>i;i+=2)
-            sum+=nums[i];
+        }
+        sort(nums.begin(), nums.end());
+        int sum = 0;
+        for(int i = 0; nums.size() > i; i+=2)
+            sum += nums[i];
         return sum;
     }
 };
