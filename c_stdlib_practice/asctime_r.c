5b287d08194119bb1c2d54e718c23446dac1c3f8
夫 <46125548+thankdesert@users.noreply.github.com>
2022-07-02 12:53:25 +0900

    Create asctime_r.c

---
+++ c_stdlib_practice/asctime_r.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <time.h>
+
+
+int main(void) {
+	/*
+	 * char *asctime(const struct tm *timeptr)
+	 * Struct -> C-string
+	 * Return Foramt :
+	 * Www Mmm dd hh:mm:ss yyyy.
+	 */
+	time_t		 rawtime;
+	struct tm	*timeinfo;
+	char		string_time[50];
+
+	/*
+	 * time_t time(time_t *timeptr);
+	 * Returns the current calendar time
+	 */
+	time(&rawtime);
+
+	/*
+	 * Converts timeval to a structure of type tm.
+	 */
+	timeinfo = localtime(&rawtime);
+	/*
+	 * Prints a current time
+	 * The string is followed b y a new_line char and
+	 * terminated with a null-char.
+	 * and the message is written into user-provided storage buffer.
+	 */
+	asctime_r(timeinfo, string_time);
+	 //printf("The currnet time is : %s", asctime_r(timeinfo, string_time));
+
+	printf("buf: %s\n", string_time);
+}
