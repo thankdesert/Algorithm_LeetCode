e5875afad6c8b1e7ab7655f78082c7e27d151993
夫 <46125548+thankdesert@users.noreply.github.com>
2022-08-01 20:55:53 +0900

    Update Unique_Paths.cpp

--- medium/Unique_Paths.cpp
+++ medium/Unique_Paths.cpp
@@ -1,6 +1,7 @@
 //from : https://leetcode.com/problems/unique-paths/
 
 
+/*
 class Solution {
 public:
     int uniquePaths(int m, int n) {
@@ -12,5 +13,25 @@ public:
         }
         
         return v[m - 1][n - 1];
+    }
+};
+*/
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int dp[m][n];
+        
+        for (int i = 0; i < n; i++)
+            dp[0][i] = 1;
+        
+        for (int i = 0; i < m; i++)
+            dp[i][0] = 1;
+        
+        for (int i = 1; i < n; i++)
+            for (int j = 1; j < m; j++)
+                dp[j][i] = dp[j-1][i] + dp[j][i-1];
+        
+        return dp[m-1][n-1];
     }
 };
