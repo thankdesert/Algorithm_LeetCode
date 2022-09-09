dc467de6c1c6a91cc65098d1b479452a3ce2948f
夫 <46125548+thankdesert@users.noreply.github.com>
2022-09-09 13:02:10 +0900

    Update Climbing_Stairs.php

--- PHP/Easy/Climbing_Stairs.php
+++ PHP/Easy/Climbing_Stairs.php
@@ -8,7 +8,11 @@ class Solution {
      * @param Integer $n
      * @return Integer
      */
+    static $memo = [];
+    
     function climbStairs($n) {
+        /* this is dp solution */
+        /*
         $dp = array_fill(0, $n + 1, 0);
         $dp[0] = 1; //first step
         for($i = 0; $i < $n; $i++) {
@@ -16,5 +20,19 @@ class Solution {
             $dp[$i + 2] += $dp[$i];
         }
         return $dp[$n];
+        */
+        
+        /* do not process this n level func call when it was already caculated */
+        if(isset(static::$memo[$n])){
+            return static::$memo[$n];
+        }
+        
+        if($n == 1 || $n == 0){
+           return 1; 
+        }
+        
+        $num = $this->climbStairs($n-1) + $this->climbStairs($n-2);
+        static::$memo[$n] = $num;
+        return $num;
     }
 }
