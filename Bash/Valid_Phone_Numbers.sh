819161612a828b508ca458bc5a57d53f3bf2f39f
夫 <46125548+thankdesert@users.noreply.github.com>
2022-09-27 10:42:43 +0900

    Create Valid_Phone_Numbers.sh

---
+++ Bash/Valid_Phone_Numbers.sh
@@ -0,0 +1,12 @@
+//from : https://leetcode.com/problems/valid-phone-numbers/
+
+
+#!/usr/bin/env bash
+
+while read line;
+do
+    if [[ "$line" =~ ^((\([0-9]{3}\) )|[0-9]{3}-)[0-9]{3}-[0-9]{4}$ ]];
+    then
+        echo $line
+    fi
+done < "file.txt"
